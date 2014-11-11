8a1a9b3cec5d1f347d8945d5016dfb9ee7092b27
Ron van der Wijngaard <rvdwijngaard@hotmail.com>
2014-11-11 10:41:10 +0100

    fix the compilation error

--- src/app/accounts/accounts.js
+++ src/app/accounts/accounts.js
@@ -2,4 +2,4 @@
 
 .controller('accountsCtrl', function (
 
-    ) { })
+    ) { });
--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -1,17 +1,17 @@
-describe( 'AppCtrl', function() {
+describe( 'ApplicationController', function() {
   describe( 'isCurrentUrl', function() {
-    var AppCtrl, $location, $scope;
+    var ApplicationController, $location, $scope;
 
-    beforeEach( module( 'ngBoilerplate' ) );
+    beforeEach( module( 'rvAdminApp' ) );
 
     beforeEach( inject( function( $controller, _$location_, $rootScope ) {
       $location = _$location_;
       $scope = $rootScope.$new();
-      AppCtrl = $controller( 'AppCtrl', { $location: $location, $scope: $scope });
+      ApplicationController = $controller( 'ApplicationController', { $location: $location, $scope: $scope });
     }));
 
     it( 'should pass a dummy test', inject( function() {
-      expect( AppCtrl ).toBeTruthy();
+      expect( ApplicationController ).toBeTruthy();
     }));
   });
 });
